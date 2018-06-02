fb00db28383560aca5b3dc468349553258bcafd3
rudolfo schepp <rudolfo.schepp@armateus.com.br>
2018-06-02 09:02:54 -0300

    Resolucao

---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QStyle>
+#include <QDesktopWidget>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            this->size(),
+            qApp->desktop()->availableGeometry()
+        )
+    );
+
+    this->showFullScreen();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
